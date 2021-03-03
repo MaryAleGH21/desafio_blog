098518ebe8228b4563cfd01ee7cd66aa93401553
Alejandra <marialesr.17@gmail.com>
2021-03-03 16:47:32 -0600

    finished project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/dashboard'
   get 'posts/create'
+  post 'posts', to: 'posts#create'
+  get 'posts/success', to: 'posts#success', as: 'success'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.destroy_all
+
+Post.create(title:'Hablemos sobre tecnología', content:'Nam vitae nisl eget nisl hendrerit finibus at a nibh. Proin sit amet auctor lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at massa non lorem suscipit tempor vel non tellus. Aenean euismod nunc consectetur rhoncus placerat.', image:'https://www.webnode.es/blog/files/2018/11/What-to-write.jpg') 
+
+Post.create(title:'Conoce más sobre escritorios de oficina', content:'Vestibulum hendrerit tortor magna, eu ultricies tellus dignissim eu. Nullam egestas odio ipsum, eu rhoncus lacus aliquam eget. Cras ut tincidunt erat. Quisque at facilisis odio, ut consectetur nulla. Cras a iaculis lectus, sit amet pharetra nisl. Sed gravida erat nec nisl fermentum, id aliquam nisl efficitur. Sed viverra egestas nunc, eu porttitor magna maximus et. Cras vel ornare metus.', image:'https://d26lpennugtm8s.cloudfront.net/stores/982/078/products/01-copia1-4c38d22f703e2dc75016003825330049-1024-1024.jpg')
+
+Post.create(title:'Audífonos Modernos', content:'Morbi a blandit metus. Nulla at scelerisque tortor, aliquam congue purus. Sed eget nunc sit amet ligula convallis hendrerit eu eu augue. Aenean ultrices quam at vestibulum rutrum. Sed nisi arcu, placerat nec fringilla volutpat, congue in ligula. Praesent dictum semper arcu non vestibulum. Donec fringilla non felis at dictum. Praesent egestas libero eu finibus alique.t', image:'https://icdn4.digitaltrends.com/image/digitaltrends_es/audifonos-airpods-pro-500x500.jpg')
